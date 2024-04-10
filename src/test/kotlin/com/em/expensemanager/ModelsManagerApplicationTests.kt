c94404aec90bd740db9bf4b175595e149b427b0d
BrunoSDomingues <brunao.signorelli@hotmail.com>
2024-04-09 21:47:24 -0300

    Test files

--- src/test/kotlin/com/em/expensemanager/ModelsManagerApplicationTests.kt
+++ src/test/kotlin/com/em/expensemanager/ModelsManagerApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ExpenseManagerApplicationTests {
+class ModelsManagerApplicationTests {
 
 	@Test
 	fun contextLoads() {
