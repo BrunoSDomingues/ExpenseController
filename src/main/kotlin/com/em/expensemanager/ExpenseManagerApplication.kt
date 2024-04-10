35032811cee23e330362e62dedcc75249d6f0017
BrunoSDomingues <brunao.signorelli@hotmail.com>
2024-04-09 21:47:34 -0300

    Main app

--- src/main/kotlin/com/em/expensemanager/ExpenseManagerApplication.kt
+++ src/main/kotlin/com/em/expensemanager/ExpenseManagerApplication.kt
@@ -1,11 +1,28 @@
 package com.em.expensemanager
 
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
 
 @SpringBootApplication
 class ExpenseManagerApplication
 
 fun main(args: Array<String>) {
 	runApplication<ExpenseManagerApplication>(*args)
+}
+
+@Configuration
+class AppConfiguration {
+	@Autowired
+	private val mongoConverter: MappingMongoConverter? = null
+
+	// remove _class
+	@PostConstruct
+	fun setupMongoEscapeCharacterConversion() {
+		mongoConverter!!.setTypeMapper(DefaultMongoTypeMapper(null))
+	}
 }
