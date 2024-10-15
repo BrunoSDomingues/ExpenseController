64203bd08eccb654d7eb0976f5fcfb8012ac8014
BrunoSDomingues <brunao.signorelli@hotmail.com>
2024-10-14 22:55:29 -0300

    Removed empty line

--- src/main/kotlin/com/em/expensemanager/Repo.kt
+++ src/main/kotlin/com/em/expensemanager/Repo.kt
@@ -3,6 +3,5 @@ package com.em.expensemanager
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface Repo : MongoRepository<Expense, String> {
-
     fun findByExpenseId(expenseId: String): Expense?
 }
