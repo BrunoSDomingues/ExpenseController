c489ca2e9d2a71847fffdf02313a1ef46fb04de1
BrunoSDomingues <brunao.signorelli@hotmail.com>
2024-10-14 22:54:46 -0300

    Updated Expense document

--- src/main/kotlin/com/em/expensemanager/Models.kt
+++ src/main/kotlin/com/em/expensemanager/Models.kt
@@ -1,5 +1,6 @@
 package com.em.expensemanager
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -8,13 +9,15 @@ import org.springframework.data.mongodb.core.mapping.Field
 @Document("expenses")
 data class Expense (
     @Id
+    @JsonIgnore
     val id: ObjectId = ObjectId(),
     val description: String = "",
+    val place: String = "",
     val amount: Double = 0.0,
     @Field("expense_id")
     val expenseId: String = ""
 ) {
     override fun toString(): String {
-        return "Expense(description='$description', amount=R$ $amount)"
+        return "Expense(description='$description', amount=R$ $amount, place=$place, expenseId=$expenseId)"
     }
 }
