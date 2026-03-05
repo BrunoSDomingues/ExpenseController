e4504607cb2f7cb472107e07084983ea86b44360
BrunoSDomingues <brunao.signorelli@hotmail.com>
2026-03-04 23:55:50 -0300

    Updated repo for new endpoints

--- src/main/kotlin/com/em/expensemanager/Repo.kt
+++ src/main/kotlin/com/em/expensemanager/Repo.kt
@@ -1,7 +1,11 @@
 package com.em.expensemanager
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import java.time.LocalDateTime
 
 interface Repo : MongoRepository<Expense, String> {
-    fun findByExpenseId(expenseId: String): Expense?
+    fun findByCreatedAtBetween(start: LocalDateTime, end: LocalDateTime): List<Expense>
+    fun findByCategory(category: String): List<Expense>
+    fun findByPaymentMethod(paymentMethod: String): List<Expense>
+    fun findByCategoryAndCreatedAtBetween(category: String, start: LocalDateTime, end: LocalDateTime): List<Expense>
 }
