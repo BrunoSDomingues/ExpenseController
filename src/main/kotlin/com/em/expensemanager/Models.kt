263442003c1d1db5738875dfed16888fbb633323
BrunoSDomingues <brunao.signorelli@hotmail.com>
2026-03-04 23:55:35 -0300

    Updated the expense model to include new fields and remove expense_id

--- src/main/kotlin/com/em/expensemanager/Models.kt
+++ src/main/kotlin/com/em/expensemanager/Models.kt
@@ -1,23 +1,35 @@
 package com.em.expensemanager
 
-import com.fasterxml.jackson.annotation.JsonIgnore
+import java.time.LocalDateTime
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.Field
 
 @Document("expenses")
-data class Expense (
+data class Expense(
     @Id
-    @JsonIgnore
+    @field:JsonSerialize(using = ToStringSerializer::class)
+    @field:JsonProperty(access = JsonProperty.Access.READ_ONLY)
     val id: ObjectId = ObjectId(),
     val description: String = "",
     val place: String = "",
     val amount: Double = 0.0,
-    @Field("expense_id")
-    val expenseId: String = ""
+    val category: String = "",
+    val paymentMethod: String = "",
+    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm")
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm")
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 ) {
+    val formattedAmount: String
+        get() = "R$ $amount"
+
     override fun toString(): String {
-        return "Expense(description='$description', amount=R$ $amount, place=$place, expenseId=$expenseId)"
+        return "Expense(description='$description', amount=R$ $amount, place=$place, " +
+                "createdAt=$createdAt, updatedAt=$updatedAt)"
     }
 }
